a5b0a60a85b893850c948a2ddffcaf677a9729b9
Randa Alhindi <randa@Randas-iMac.local>
2019-08-24 03:25:08 -0700

    day 2

---
+++ day 2.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var str = "Hello, playground"
+let age: Int = 29
+let pi: Double = 3.14159265
+let GPA: Float = 3.53
+let size: Character = "M"
+let name: String = "Randa"
+let raining: Bool = false
+
+
+//Both variables are the same
+var rainingoutside: Bool = false
+
+// it started raining
+rainingoutside = true
+
+
+var eggPrice: Int = 15
+var amountOfEggsBought : Int = 3
+var totalAmount = eggPrice * amountOfEggsBought
+print("soubtotal:\(totalAmount)")
+//output: subtotal = 45
+
+
+
+
