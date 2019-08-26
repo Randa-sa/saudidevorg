33a9a978d07d82372164e3c16274de25ed07c6b5
Randa Alhindi <randa@Randas-iMac.local>
2019-08-26 05:40:25 -0700

    day 5 quiz

---
+++ day 5 quiz.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// first question
+let person1 = "Randa"
+let salary = 5000
+let housing = 1000
+let living = 700
+let others = 1300
+let year = 12
+
+var AnnualSalary = salary * year
+var AnnualExpenses = (housing + living + others) * year
+var AnnualSavings = AnnualSalary - AnnualExpenses
+
+// another way
+ AnnualSalary = 5000 * 12
+ AnnualExpenses = (1000 + 700 + 1300) * 12
+ AnnualSavings = 60000 - 36000
+
+// Second question
+
+var sign = "Smoking is not allowed" // String
+var ag = 22 // Int
+var speed = 54.7 // Douple
+var iLoveSwift = true //Bool
+
+// Third question
+var age = 12
+let allowedToEnter = age >= 18
+
+// 18 or older
+
+5 + 5 != 10 // false
+
+let goWatchMovie = (85 > 90 && 75 > 80) || true // true
+
+
+
+
+
