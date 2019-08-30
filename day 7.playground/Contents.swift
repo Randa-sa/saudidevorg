2a0cfd2bbfea25f4bc74d0aa247edf85f08d9bc1
Randa Alhindi <randa@Randas-iMac.local>
2019-08-30 13:31:09 -0700

    day 7+8

---
+++ day 7.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+// for in loop
+for n in 1...5 {
+print (n)
+}
+
+for n in 1..<5 {
+    print (n)
+}
+for _ in 1 ... 100 {
+    print ("Saudi Arabia")
+}
---
+++ day 8.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var str = "Hello, playground"
+// while loop
+var i = 1
+while i <= 100 {
+    print ("SaudiVision2030")
+ i += 1
+}
+
+var countdown = 10
+repeat {
+    print (countdown)
+    countdown = 0
+} while countdown > 0
+// 10
+
+var MyAge = 29
+
+while true{
+    print (MyAge)
+    if MyAge == 29 {
+        break
+    }
+}
+
